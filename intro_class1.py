b6ea3acbd4959b0ba9b7999e860e795daa2f7626
Al <albertosshi@gmail.com>
2022-01-31 15:19:44 +0100

    Create intro_class1.py

---
+++ intro_class1.py
@@ -0,0 +1,9 @@
+class Dog:
+  dog_time_dilation = 7
+ 
+  def time_explanation(self):
+    print("Dogs experience {} years for every 1 human year.".format(self.dog_time_dilation))
+ 
+pipi_pitbull = Dog()
+pipi_pitbull.time_explanation()
+# Prints "Dogs experience 7 years for every 1 human year."
